888b302da3891313e859b09084504c31ea02b3ea
“xie” <hex2025@163.com>
2019-03-27 16:58:49 +0800

    增加UserController

---
+++ src/main/java/com/example/demo/controller/UserConterller.java
@@ -0,0 +1,11 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xiemin
+ * @date 2019/3/27 16:57
+ */
+@RestController
+public class UserConterller {
+}
